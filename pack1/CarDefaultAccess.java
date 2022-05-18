418f978828df3340723096482ebcdca7eb28ba3e
admhercull <103071088+admhercull@users.noreply.github.com>
2022-05-18 16:35:16 +0200

    Create CarDefaultAccess.java

---
+++ pack1/CarDefaultAccess.java
@@ -0,0 +1,14 @@
+// Java program to illustrate DEFAULT MODIFIER. Default access is the modifier by default: 
+// Methods & vbles accessibles only in SAME CLASS & SAME PACKAGE
+package pack1;
+
+// Class CarDefaultAccess has Default access modifier by default
+// default class CarDefaultAccess
+public class CarDefaultAccess
+
+{
+	public void display()
+	{
+		System.out.println("CarDefaultAccess");
+	}
+}
